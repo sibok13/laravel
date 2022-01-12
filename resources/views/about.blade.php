298afdd70f8fca577e2f280e4e2417978735e207
Sibok <maelstrom@mail.ru>
2022-01-12 22:43:33 +0400

    HW01

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>О проекте</title>
+</head>
+<body>
+    <h1>О проекте</h1>
+    <p>Это учебный проект по освоению Framework LARAVEL на курсе Вэб-разработки в Geek Brance</p>
+    <p><a href="hello">Главная страница</a></p>
+    <p><a href="news">Новости проекта</a></p>
+</body>
+</html>
---
+++ resources/views/news.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Новости проекта</title>
+</head>
+<body>
+    <h1>Новости проекта</h1>
+    <div style="width: 100%; border: 1px solid black; padding: 20px; margin-bottom: 30px;">
+        <h2>Новость 1</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa omnis temporibus perspiciatis magnam, eum illum voluptate consectetur ut consequuntur nemo ad! Sed tempore nulla cupiditate eius dolorum nam porro culpa.</p>
+        <a href="news1">Подробнее</a>
+    </div>
+    <div style="width: 100%; border: 1px solid black; padding: 20px; margin-bottom: 30px;">
+        <h2>Новость 2</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa omnis temporibus perspiciatis magnam, eum illum voluptate consectetur ut consequuntur nemo ad! Sed tempore nulla cupiditate eius dolorum nam porro culpa.</p>
+        <a href="news1">Подробнее</a>
+    </div>
+    <div style="width: 100%; border: 1px solid black; padding: 20px; margin-bottom: 30px;">
+        <h2>Новость 3</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa omnis temporibus perspiciatis magnam, eum illum voluptate consectetur ut consequuntur nemo ad! Sed tempore nulla cupiditate eius dolorum nam porro culpa.</p>
+        <a href="news1">Подробнее</a>
+    </div>
+    <p><a href="hello">Главная страница</a></p>
+    <p><a href="about">Подробнее о проекте</a></p>
+</body>
+</html>
